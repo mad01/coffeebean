312e5cf6831c51a7b5c6a3ce54ff4b14d587816a
Alexander Brandstedt <alexander@infralium.com>
2018-03-15 11:51:46 +0100

    adding working example

--- cmd.go
+++ cmd.go
@@ -18,9 +18,31 @@ func cmdVersion() *cobra.Command {
 	return command
 }
 
+
+func cmdApply() *cobra.Command {
+	var filename string
+	var kubeconfig string
+	var command = &cobra.Command{
+		Use:   "apply",
+		Short: "apply k8s manifest file",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			client := newkClient(kubeconfig)
+			err := client.apply(filename)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		},
+	}
+	command.Flags().StringVarP(&kubeconfig, "kube.config", "k", "", "outside cluster path to kube config")
+	command.Flags().StringVarP(&filename, "filename", "f", "", "path to dir/file/url")
+	return command
+}
+
 func runCmd() error {
 	var rootCmd = &cobra.Command{Use: "kubectl-client"}
 	rootCmd.AddCommand(cmdVersion())
+	rootCmd.AddCommand(cmdApply())
 
 	err := rootCmd.Execute()
 	if err != nil {
---
+++ kclient.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/mad01/node-terminator/pkg/kutil"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/kubernetes/pkg/kubectl/cmd"
+	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+	"k8s.io/kubernetes/pkg/kubectl/resource"
+)
+
+func newkClient(kubeconfig string) *kClient {
+	config, err := k8sGetClientConfig(kubeconfig)
+	if err != nil {
+		panic(fmt.Sprintf("failed to get kube rest config: %v", err.Error()))
+	}
+
+	e := kClient{
+		waitInterval: 1 * time.Minute,
+		ClientConfig: kutil.NewClientConfig(config, metav1.NamespaceAll),
+	}
+	return &e
+}
+
+// kClient struct
+type kClient struct {
+	waitInterval time.Duration
+	ClientConfig clientcmd.ClientConfig
+}
+
+func (e *kClient) apply(path string) error {
+	f := cmdutil.NewFactory(e.ClientConfig)
+
+	options := &cmd.ApplyOptions{
+		FilenameOptions: resource.FilenameOptions{
+			Filenames: []string{path},
+		},
+		Cascade: true,
+	}
+
+	// schema, err := f.Validator(cmdutil.GetFlagBool(cmd, "validate"), cmdutil.GetFlagBool(cmd, "openapi-validation"), cmdutil.GetFlagString(cmd, "schema-cache-dir"))
+	cobraCmd := &cobra.Command{
+		Use: "apply",
+	}
+	cobraCmd.Flags().Bool("validate", true, "")
+	cobraCmd.Flags().Bool("openapi-validation", true, "")
+	cobraCmd.Flags().Bool("dry-run", false, "")
+	cobraCmd.Flags().Bool("overwrite", true, "")
+	cobraCmd.Flags().Bool("record", false, "")
+	cobraCmd.Flags().String("schema-cache-dir", "", "")
+	cobraCmd.Flags().String("output", "", "")
+
+	err := cmd.RunApply(
+		f,
+		cobraCmd,
+		os.Stdout,
+		os.Stderr,
+		options,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to run apply: %v", err.Error())
+	}
+
+	return nil
+}
+
